c4be61ccf9b2eed39359cd1c4f80295afd3003f9
328295484@qq.com <wht@123456>
2020-04-07 17:36:27 +0800

    基本脚手架代码

--- user-service/src/main/java/com/xl/ems/userservice/common/RestCode.java
+++ user-service/src/main/java/com/xl/ems/userservice/common/RestCode.java
@@ -3,8 +3,11 @@ package com.xl.ems.userservice.common;
 public enum RestCode {
 
     OK(0, "OK"),
-    UNKNOWN_ERROR(1, "服务异常"),
-    WRONG_PAGE(10100, "页码不存在");
+    UNKNOWN_ERROR(1,"未知异常"),
+    TOKEN_INVALID(2,"TOKEN失效"),
+    USER_NOT_EXIST(3,"用户不存在"),
+    WRONG_PAGE(10100,"页码不合法"),
+    LACK_PARAMS(10101,"缺少参数");
 
     public final int code;
 
